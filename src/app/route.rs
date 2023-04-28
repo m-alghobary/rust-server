75e6b8723f9dbb647e4d2427223e6175be07abf9
m-alghobary <m.alghobary90@gmail.com>
2023-04-28 20:53:29 +0300

    add unit tests for route struct

--- src/app/route.rs
+++ src/app/route.rs
@@ -39,3 +39,59 @@ impl Route {
         params
     }
 }
+
+/// Unit Tests
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    fn init_route(path: &str) -> Route {
+        let handler: RouteHandler = Arc::new(|_r: Request| -> Response { todo!() });
+        Route::new(HttpMethod::Get, path.to_owned(), handler)
+    }
+
+    #[test]
+    fn get_params_work_with_no_param() {
+        let path = "/posts/all";
+        let route = init_route(path);
+
+        let params = route.get_params();
+
+        assert_eq!(params.len(), 0);
+    }
+
+    #[test]
+    fn get_params_work_with_one_param() {
+        let path = "/post/{id}/comments";
+        let route = init_route(path);
+
+        let params = route.get_params();
+
+        assert_eq!(params.len(), 1);
+        assert_eq!(params[0], (2, "id"));
+    }
+
+    #[test]
+    fn get_params_work_with_just_param() {
+        let path = "/{id}";
+        let route = init_route(path);
+
+        let params = route.get_params();
+
+        assert_eq!(params.len(), 1);
+        assert_eq!(params[0], (1, "id"));
+    }
+
+    #[test]
+    fn get_params_work_with_more_than_one_param() {
+        let path = "/post/{id}/comments/{comment_id}";
+        let route = init_route(path);
+
+        let params = route.get_params();
+
+        assert_eq!(params.len(), 2);
+        assert_eq!(params[0], (2, "id"));
+        assert_eq!(params[1], (4, "comment_id"));
+    }
+}
